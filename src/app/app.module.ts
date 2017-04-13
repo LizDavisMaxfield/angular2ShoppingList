47409ec2d848dab6b408c9b0cb580d504e311c5e
Alec <crestaniAG@mtc.byu.edu>
2017-04-13 10:11:09 -0600

    Add Shopping List App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './header-component/header-component.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { ShoppingListEditComponent } from './shopping-list-edit/shopping-list-edit.component';
+import { RecipeItemComponent } from './recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    RecipeBookComponent,
+    ShoppingListEditComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule,
